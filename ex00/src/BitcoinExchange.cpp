e2867306b00b96af156827ef812f4381ca126575
nseon <nseon@student.42lyon.fr>
2026-02-20 11:19:27 +0100

    AAAAAAAAAAAAA all my fix are gone

--- ex00/src/BitcoinExchange.cpp
+++ ex00/src/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: nseon <nseon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/26 16:52:53 by nseon             #+#    #+#             */
-/*   Updated: 2026/02/20 11:16:15 by nseon            ###   ########.fr       */
+/*   Updated: 2026/02/20 11:18:15 by nseon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -102,7 +102,7 @@ static float parse_nb(std::string line, int nb_line, std::string file_name)
 	ss >> nb;
 	if (ss.fail())
 		throw BuildError(nb_line, "Don't fit in a float: " + line, file_name);
-	if (nb > 1000)
+	if (file_name != DATAFILE && nb > 1000)
 		throw BuildError(nb_line, "Number is too big: " + line, file_name);
 	if (nb < 0)
 		throw BuildError(nb_line, "Not a positive number: " + line, file_name);
